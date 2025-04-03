83cb88edece604b11d73f972e3396ecfe3360aac
Maik <141837531+asshoffm@users.noreply.github.com>
2025-04-03 13:40:15 +0200

    Prevent scheduling polling attempts that would exceed timeout (#1)
    * Prevent scheduling polling attempts that would exceed timeout
    
    * Clean up code (use interval already computed)
    
    * Revert adjustments to interval lengths.

--- Sources/AsyncPoller/AsyncPoller.swift
+++ Sources/AsyncPoller/AsyncPoller.swift
@@ -68,11 +68,13 @@ public actor AsyncPoller<T: Sendable> {
                 return result
             }
 
-            if let startTime = startTime, Date.now.timeIntervalSince(startTime) > configuration.timeoutInterval {
+            let nextInterval = configuration.pollingInterval(iteration: iteration)
+
+            if let startTime = startTime, Date.now.timeIntervalSince(startTime) + nextInterval > configuration.timeoutInterval {
                 throw PollingError.timeout
             }
 
-            try await Task.sleep(nanoseconds: UInt64(configuration.pollingInterval(iteration: iteration) * 1_000_000_000))
+            try await Task.sleep(nanoseconds: UInt64(nextInterval * 1_000_000_000))
         }
         throw CancellationError()
     }
